aaba7e7075b5558f2e34941c1309eb11cdf17bcb
Muhammad Abdur Rafey <115175167+MARafey@users.noreply.github.com>
2025-04-05 10:51:00 +0500

    Update normal.cpp

--- Files/normal.cpp
+++ Files/normal.cpp
@@ -6,9 +6,6 @@ using namespace std;
 void multiplyMatrices(vector<vector<int>> &A, vector<vector<int>> &B, vector<vector<int>> &C, int N) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            for (int ll = 0; ll < N*N; ll++){
-
-            }
             C[i][j] = 0;
             for (int k = 0; k < N; k++) {
                 C[i][j] += A[i][k] * B[k][j];
@@ -33,13 +30,6 @@ int main() {
     }
 
     multiplyMatrices(A, B, C, N);
-
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            cout << C[i][j] << " ";
-        }
-        cout << endl;
-    }
 
     return 0;
 }
